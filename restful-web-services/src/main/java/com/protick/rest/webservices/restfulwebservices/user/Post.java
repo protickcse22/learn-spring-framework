2dfb182f74e194eb8be3e3a696b00b44c1a8aafc
protick.kumer <protick.kumer@bjitgroup.com>
2023-07-21 13:58:33 +0600

    - create post request for post resource.  - Get specific post for specific user.  - Add mysql database  - Added basic security

--- restful-web-services/src/main/java/com/protick/rest/webservices/restfulwebservices/user/Post.java
+++ restful-web-services/src/main/java/com/protick/rest/webservices/restfulwebservices/user/Post.java
@@ -2,6 +2,7 @@ package com.protick.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Post {
@@ -9,6 +10,7 @@ public class Post {
     @Id
     @GeneratedValue
     private Integer id;
+    @Size(min = 10)
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -32,4 +34,20 @@ public class Post {
     }
 
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
--- restful-web-services/src/main/java/com/protick/rest/webservices/restfulwebservices/user/UserJpaResource.java
+++ restful-web-services/src/main/java/com/protick/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -1,6 +1,8 @@
 package com.protick.rest.webservices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +12,15 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 @RestController
 public class UserJpaResource {
 
     private UserRepository userRepository;
     private PostRepository postRepository;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public UserJpaResource(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
@@ -62,4 +67,34 @@ public class UserJpaResource {
         }
         return user.get().getPosts();
     }
+
+    @PostMapping(path = "/jpa/users/{id}/posts")
+    public ResponseEntity<Post> createPost(@PathVariable int id,@Valid @RequestBody Post post) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("User id: " + id);
+        }
+        post.setUser(user.get());
+        Post savedPost = postRepository.save(post);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPost.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+
+//    http://localhost:8080/jpa/users/10001/posts/1
+    @GetMapping(path = "/jpa/users/{user_id}/posts/{post_id}")
+    public Post retrieveUserSpecificPost(@PathVariable int user_id, @PathVariable int post_id) {
+        Optional<User> user = userRepository.findById(user_id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("User id: " + user_id);
+        }
+        Predicate<? super Post> filter = post -> post.getId() == post_id;
+        Post post = user.get().getPosts().stream().filter(filter).findFirst().orElse(null);
+        logger.info("Post: "+post);
+        return post;
+    }
+
 }
