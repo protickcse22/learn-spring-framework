98e6d0db8000f7e7ba40daba3673127f97c06252
protick.kumer <protick.kumer@bjitgroup.com>
2023-07-26 12:03:33 +0600

    - Refactor the folder structure - Added backend spring boot project - Added rest client axios and call hello world api

--- Full-Stack/front-end/toto-app/src/components/todo/WelcomeComponent.jsx
+++ Full-Stack/front-end/toto-app/src/components/todo/WelcomeComponent.jsx
@@ -1,13 +1,63 @@
+import axios from "axios"
+import { useState } from "react"
 import { Link, useParams } from "react-router-dom"
+import { retrieveHelloWorld, retrieveHelloWorldBean, retrieveHelloWorldPathVariable } from "./api/HelloWorldApiService"
 
 function WelcomeComponent() {
     const { username } = useParams()
+
+    const [message, setMessage] = useState(null)
+
+
+    function callHelloWorldRestApi() {
+
+        retrieveHelloWorldPathVariable('protick')
+            .then(
+                (response) => successFulResponse(response)
+            )
+            .catch(
+                (error) => errorResponse(error)
+            )
+            .finally(
+                console.log('cleanup')
+            )
+
+
+        // retrieveHelloWorld()
+        //     .then(
+        //         (response) => successFulResponse(response)
+        //     )
+        //     .catch(
+        //         (error) => errorResponse(error)
+        //     )
+        //     .finally(
+        //         console.log('cleanup')
+        //     )
+    }
+
+    function successFulResponse(response) {
+        console.log(response.data.message)
+        setMessage(response.data.message)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
+
+
+
+
     return (
         <div className="Welcome">
             <h1>Welcome to {username}</h1>
             <div>
                 Manage your todos - <Link to='/todos'>Go here</Link>
             </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
